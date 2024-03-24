232933b715827f973d36a596e79e17639927dfeb
BishalParajuli2057 <bishalparajuli2057@gmail.com>
2024-03-24 19:59:53 +0200

    2

--- app/src/main/java/com/example/week_10/AddUserActivity.java
+++ app/src/main/java/com/example/week_10/AddUserActivity.java
@@ -16,7 +16,7 @@ public class AddUserActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_add_user);
 
         firstname = findViewById(R.id.textFirstName);
         lastname = findViewById(R.id.textLastName);
